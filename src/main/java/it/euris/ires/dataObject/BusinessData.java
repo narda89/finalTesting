18385a980f87e9bed7b44df6673a992a9780a542
narda89 <nardanarda89@gmail.com>
2021-07-20 17:02:18 +0200

    partial

--- src/main/java/it/euris/ires/dataObject/BusinessData.java
+++ src/main/java/it/euris/ires/dataObject/BusinessData.java
@@ -1,8 +1,11 @@
 package it.euris.ires.dataObject;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+
 public class BusinessData {
 
 	private String accountId;
--- src/test/java/it/euris/ires/business/PaySessionBusinessTest.java
+++ src/test/java/it/euris/ires/business/PaySessionBusinessTest.java
@@ -1,8 +1,6 @@
 package it.euris.ires.business;
 
-import it.euris.ires.dataObject.BusinessData;
-import it.euris.ires.dataObject.CreatePaySessionRequest;
-import it.euris.ires.dataObject.CreatePaySessionResponse;
+import it.euris.ires.dataObject.*;
 import it.euris.ires.entity.PaySession;
 import it.euris.ires.exception.PaySessionException;
 import it.euris.ires.service.IPaymentSessionService;
@@ -14,6 +12,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -39,13 +40,19 @@ class PaySessionBusinessTest {
         // ARRANGE
         paySessionBusiness = new PaySessionBusiness(paymentSessionServiceMocked);
         CreatePaySessionRequest request = new CreatePaySessionRequest();
-        //PaySession paySessionGood = new PaySession();
-        PaySession paySession = paymentSessionServiceMocked.createWebPaySession(request);
-        Mockito.when(paymentSessionServiceMocked.createWebPaySession(any())).thenReturn(paySession);
+        SaleItem item1 = new SaleItem("name","description","productid","variantid","1000", 2);
+        List<SaleItem> items = new ArrayList<>();
+        items.add(item1);
+        request.setSaleItems(items);
+        request.setTaxAmount("10");
+        BusinessData businessData = new BusinessData("accountID", "paymentconfigID","consumerID");
+        request.setBusinessData(businessData);
+        PaySessionSettings paySessionSettings = new PaySessionSettings(10,false,false,"euro","english");
+        request.setPaySessionSettings(paySessionSettings);
         // ACT
-        paySessionBusiness.createPaySession(request);
+        CreatePaySessionResponse createPaySessionResponse= paySessionBusiness.createPaySession(request);
         // ASSERT
-        Mockito.verify(response, Mockito.times(1)).setSuccess(true);
+        //Mockito.verify(response, Mockito.times(1)).setSuccess(true);
         //Mockito.verify(response, Mockito.times(1)).setStatus(paySession.getStatus().name());
         //Mockito.verify(response, Mockito.times(1)).setPaySessionId(paySession.getUuid().toString());
     }
